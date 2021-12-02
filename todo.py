36eb57e9c50db965399b4ca6fdb7430f9ce0144d
TyB-Tech <93352444+TyB-Tech@users.noreply.github.com>
2021-12-02 23:29:31 +0000

    nice job

--- todo.py
+++ todo.py
@@ -1,1 +1,17 @@
+mytodolist = ['buy Cleats',  'buy gloves', 'practice footwork', 'wear the number 9','become the 1# Linebacker in the NFL','lift at least 500','Become the fastest Linebacker in the NFL']
 
+print(len(mytodolist))
+print(mytodolist)
+
+print (mytodolist.index('buy Cleats'))
+print("mytodolist")
+for task in mytodolist:
+  print(task)
+
+print('what would you like to do') 
+add = input()
+mytodolist.append(add)
+remove = input()
+mytodolist.remove(remove)
+mytodolist.remove('lift at least 500')
+print(mytodolist)
